436e8c209f4a46f0e14dad30b314aef3e676a08f
sbormann71 <69652323+sbormann71@users.noreply.github.com>
2021-01-20 18:01:15 +0100

    IIC test

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+let gelesen = 0
+input.onButtonPressed(Button.A, function () {
+    pins.i2cWriteNumber(
+    39,
+    254,
+    NumberFormat.UInt8LE,
+    false
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    pins.i2cWriteNumber(
+    39,
+    255,
+    NumberFormat.UInt8LE,
+    false
+    )
+})
+basic.forever(function () {
+    gelesen = pins.i2cReadNumber(39, NumberFormat.UInt8LE, false)
+    gelesen = BitwiseLogic.bitwise2arg(gelesen, operator.and, 2)
+    basic.showString("" + (gelesen))
+    basic.pause(BitwiseLogic.bitwise2arg(0, operator.and, 0))
+})
