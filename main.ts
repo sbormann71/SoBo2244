b6065081334325870c57ab1aa356cd3ae63dff4f
sbormann71 <69652323+sbormann71@users.noreply.github.com>
2021-01-26 22:35:37 +0100

    test IIC (toggle outputs per button, inputs on display)

--- main.ts
+++ main.ts
@@ -1,23 +1,24 @@
-let gelesen = 0
 input.onButtonPressed(Button.A, function () {
+    Ausgang = Ausgang * 2
+    if (Ausgang > 8) {
+        Ausgang = 1
+        basic.setLedColor(0xff0000)
+    } else {
+        basic.setLedColor(0x00ff00)
+    }
     pins.i2cWriteNumber(
     39,
-    254,
+    Ausgang + 112,
     NumberFormat.UInt8LE,
     false
     )
 })
-input.onButtonPressed(Button.B, function () {
-    pins.i2cWriteNumber(
-    39,
-    255,
-    NumberFormat.UInt8LE,
-    false
-    )
-})
+let gelesen = 0
+let Ausgang = 0
+Ausgang = 1
 basic.forever(function () {
     gelesen = pins.i2cReadNumber(39, NumberFormat.UInt8LE, false)
-    gelesen = BitwiseLogic.bitwise2arg(gelesen, operator.and, 2)
-    basic.showString("" + (gelesen))
-    basic.pause(BitwiseLogic.bitwise2arg(0, operator.and, 0))
+    gelesen = BitwiseLogic.bitwise2arg(gelesen, operator.rightShift, 4)
+    basic.showString("" + (BitwiseLogic.bitwise2arg(BitwiseLogic.bitwiseNot(gelesen), operator.and, 7)))
+    basic.pause(1000)
 })
